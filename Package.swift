552f466ea686e372383c59374e60fd554f718478
autobuild <autobuild@m2mini.inner.acrobits.cz>
2024-09-04 11:10:01 +0200

    Package updated to 1.1.4

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/heartbeat-swift-package/release/Softphone.xcframework-1.1.3.zip",
-            checksum: "ec1ea3ef3b0bc94eb8171e75d963bcbc0e3f4278e51cbae47f625f509ec1d752"),
+            url: "https://dist.acrobits.net/heartbeat-swift-package/release/Softphone.xcframework-1.1.4.zip",
+            checksum: "4f5809c2e7ecfe6854c60786a0ecf0a76ab49d57592603d4f701663def0bd6f0"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/heartbeat-swift-package/release/Softphone_Swift.xcframework-1.1.3.zip",
-            checksum: "b669ac2f3e06aba1f8908ac30072fa70d1f3d50a3d069d188bcfedf2bef3424b"),
+            url: "https://dist.acrobits.net/heartbeat-swift-package/release/Softphone_Swift.xcframework-1.1.4.zip",
+            checksum: "9f087d3c2e8b76caa99f5dc32cdbca34bf02344fc0f6d71e9206f5b06661ca90"),
     ]
 )
